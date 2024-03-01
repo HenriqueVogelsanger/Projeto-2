1e2412eb39aa5bf9da98125cc2a211f99a0bd15e
HenriqueVogelsanger <henriquevogelsanger2008@gmail.com>
2024-03-01 16:47:00 -0300

    Aula3

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { ref } from 'vue'
 const contador = ref(0)
+const contadoruhh = ref(0)
+
+const real = ref(true)
+
+
 function saudacao() {
   return `Olá, ${nome}!`
 }
@@ -22,10 +27,28 @@ function aumentar() {
 function zerar() {
   contador.value = 0
 }
+
+function diminuiruhh() {
+  if (contadoruhh.value > 0) {
+    contadoruhh.value--
+  }
+}
+function aumentaruhh() {
+
+  contadoruhh.value++;
+
+}
+function zeraruhh() {
+  contadoruhh.value = 0
+}
+
+function real1(){
+  real.value =  !real.value  
+}
 </script>
 
 <template>
-  <div class="info">
+ <!--  <div class="info">
 
     <h1>Exemplo 1 - Mostrando informações na tela</h1>
         <p>Nome: {{ nome }} </p>
@@ -37,23 +60,38 @@ function zerar() {
         <hr />
         <p> Exemplo de saudação usando função</p>
         <p> {{ saudacao() }}  </p>
-  </div>
+  </div> -->
 <div class="info">
   <button @click="aumentar">+ aumentar</button>
   <button @click="diminuir">- diminuir</button>
   <button @click="zerar">apagar</button>
   <p>O valor do contador é: {{ contador }}</p>
 </div>
+<div class="info">
+  <button @click="aumentaruhh">+ aumentar</button>
+  <button @click="diminuiruhh">- diminuir</button>
+  <button @click="zeraruhh">apagar</button>
+  <p>O valor do contador é: {{ contadoruhh }}</p>
+</div>
 
 <div class="fl" v-if=" contador >= 10" style="background-color: green;">Maior que Dez</div>
 
 <div class="fl" v-if=" contador < 10" style="background-color: red;">Menor que Dez</div>
 
+<div class="fl" v-if=" contadoruhh >= 10" style="background-color: green;">Maior que Dez</div>
 
+<div class="fl" v-if=" contadoruhh < 10" style="background-color: red;">Menor que Dez</div>
 
+<div class="fl" style="background-color: yellow;"> {{contador + contadoruhh}}</div>
 
+<div class="fl" v-if=" (contador + contadoruhh) %2 == 0 " style="background-color: blue;">O Numero é Par</div>
 
+<div class="fl" v-if=" (contador + contadoruhh) %2 != 0 " style="background-color: rgb(255, 145, 0);">O Numero é Impar</div>
 
+<button @click="real1()">
+  t
+</button>
+<p v-show="real">sodaihffgdwdef</p>
 </template>
 
 <style scoped>
@@ -61,6 +99,7 @@ function zerar() {
   padding: 20px 30px;
   color: rgb(0, 0, 0);
   border-radius: 10px;
+  margin: 1rem;
 }
 button {
   font-size: 2rem;
