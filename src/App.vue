44f546aa6a927ca5636969eac1fbb80075b14cc9
HenriqueVogelsanger <henriquevogelsanger2008@gmail.com>
2024-07-03 20:20:04 -0300

    quarta a noite

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,44 @@ import { ref, reactive } from 'vue';
 import { toast } from 'vue3-toastify'
 const nome = ref('');
 
-let dados = reactive({
+const siglas = ref([ 
+   "AC",
+   "AL",
+   "AP",
+   "AM",
+   "BA",
+   "CE",
+   "DF",
+   "ES",
+   "GO",
+   "MA",
+   "MT",
+   "MS",
+   "MG",
+   "PA",
+   "PB",
+   "PR",
+   "PE",
+   "PI",
+   "RJ",
+   "RN",
+   "RS",
+   "RO",
+   "RR",
+   "SC",                 
+   "SP",
+   "SE",
+   "TO"
+])
+
+const dados = reactive({
   nome: "",
   email: "",
-  idade: 0
-
+  dataNascimento: "",
+  senha: "",
+  confirmarSenha: "",
+ endereço: "",
+ cidade:""
 })
 
 const ver = ref(false)
@@ -26,13 +59,13 @@ const idadeValido = (idade) => {
 
 
 function enviar() {
-  if (dados.idade < 18 || dados.idade > 60) {
-    toast.warn("Idade Ruim", { autoClose: 1000 })
-  }
-  else if (dados.nome.length <= 3 || dados.nome.length >= 20) {
-    toast.warn("Nome Ruim", { autoClose: 1000 })
-  }
-  console.log(dados)
+  // if (dados.idade < 18 || dados.idade > 60) {
+  //   toast.warn("Idade Ruim", { autoClose: 1000 })
+  // }
+  // else if (dados.nome.length <= 3 || dados.nome.length >= 20) {
+  //   toast.warn("Nome Ruim", { autoClose: 1000 })
+  // }
+  ver.value = true
 }
 
 </script>
@@ -45,13 +78,20 @@ function enviar() {
       <input type="text" v-model="dados.nome" />
       <label for="email">Email:</label>
       <input type="email" v-model="dados.email" />
-      <label for="idade">Idade:</label>
-      <input type="number" v-model="dados.idade" />
+      <label for="dataNascimento">dataNascimento:</label>
+      <input type="date" v-model="dados.dataNascimento" />
       <label for="senha">Senha:</label>
       <input type="password" v-model="dados.senha" />
       <label for="senha">Confirmar Senha:</label>
       <input type="password" v-model="dados.confirmarSenha" />
-
+      <label for="endereço">Endereço:</label>
+      <input type="text" v-model="dados.endereço" />
+      <label for="cidade">Cidade:</label>
+      <input type="text" v-model="dados.cidade" />
+      <label for="estado">Estado:</label>
+      <select name="" v-model="dados.estado" id="">
+        <option  v-for="item in siglas" :value="item" :key="item" >{{ item }}</option>
+      </select>
 
       <button type="submit">Enviar</button>
     </form>
@@ -59,9 +99,12 @@ function enviar() {
       <h2>Respostas:</h2>
       <p>{{ dados.nome }}</p>
       <p>{{ dados.email }}</p>
-      <p>{{ dados.idade }}</p>
+      <p>{{ dados.dataNascimento }}</p>
       <p>{{ dados.senha }}</p>
       <p>{{ dados.confirmarSenha }}</p>
+      <p>{{ dados.endereço }}</p>
+      <p>{{ dados.cidade}}</p>
+      <p>{{ dados.estado}}</p>
     </div>
     <!-- <div v-if="invalido">
       <p>Algo de errado não está certo!</p>
